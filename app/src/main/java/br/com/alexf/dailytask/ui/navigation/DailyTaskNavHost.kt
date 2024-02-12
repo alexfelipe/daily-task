ab3b253ce170e9225f5e831169c8e4fbca1d84b4
Alex Felipe <alexfelipevieira@gmail.com>
2024-02-12 18:10:46 -0300

    Configuring the state management for task form screen

--- app/src/main/java/br/com/alexf/dailytask/ui/navigation/DailyTaskNavHost.kt
+++ app/src/main/java/br/com/alexf/dailytask/ui/navigation/DailyTaskNavHost.kt
@@ -1,14 +1,18 @@
 package br.com.alexf.dailytask.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import br.com.alexf.dailytask.dao.TaskDao
 import br.com.alexf.dailytask.ui.screens.TaskFormScreen
+import br.com.alexf.dailytask.ui.screens.TaskFormState
 import br.com.alexf.dailytask.ui.screens.TasksListScreen
 
 @Composable
@@ -30,7 +34,22 @@ fun DailyTaskNavHost(navController: NavHostController) {
             )
         }
         composable("taskForm") {
-            TaskFormScreen(onSaveClick = { task ->
+            var state by remember {
+                mutableStateOf(TaskFormState())
+            }
+            LaunchedEffect(null) {
+                state = state.copy(
+                    onTitleChange = {
+                        state = state.copy(title = it)
+                    },
+                    onDescriptionChange = {
+                        state = state.copy(description = it)
+                    }
+                )
+            }
+            TaskFormScreen(
+                state = state,
+                onSaveClick = { task ->
                 dao.save(task)
                 navController.navigateUp()
             })
